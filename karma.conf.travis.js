7a07a262225c8d4e7da628eeba5d55a62f0d9818
Vasu <skmvasu@gmail.com>
2018-01-12 21:06:40 +0530

    Travis config

---
+++ karma.conf.travis.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+module.exports = function (config) {
+  config.set({
+    files: [
+      'tests.webpack.js',
+    ],
+    frameworks: [
+      'jasmine'
+    ],
+     preprocessors: {
+      './tests.webpack.js': ['webpack', 'sourcemap'],
+    },
+
+    webpack: {
+      cache: true,
+      devtool: 'inline-source-map',
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            include: path.resolve(__dirname, '../src'),
+            exclude: /(bower_components|node_modules|__tests__)/,
+            loader: 'babel',
+            query: {
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+};
