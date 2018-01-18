ff96a6637aab410099a46abdddabecf368b5c71b
Vasu <skmvasu@gmail.com>
2018-01-18 13:00:49 +0530

    CRA for examples

---
+++ webbpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path"),
+  webpack = require("webpack"),
+  CleanWebpackPlugin = require("clean-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: "./assets/css/app.css"
+});
+
+const config = {
+  // absolute path for project root
+  context: path.resolve(__dirname, "src"),
+
+  entry: {
+    // relative path declaration
+    app: "./http.js"
+  },
+
+  output: {
+    // absolute path declaration
+    path: path.resolve(__dirname, "dist"),
+    filename: "./build/js/[name].bundle.js"
+  },
+
+  module: {
+    rules: [
+      // babel-loader with 'env' preset
+      {
+        test: /\.js$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader", options: { presets: ["env"] } }
+      },
+      // html-loader
+      { test: /\.html$/, use: ["html-loader"] }
+    ]
+  },
+
+  plugins: [
+    // cleaning up only 'dist' folder
+    new CleanWebpackPlugin(["build"]),
+    // extract-text-webpack-plugin instance
+    extractPlugin
+  ],
+
+  devtool: "inline-source-map"
+};
+
+module.exports = config;
